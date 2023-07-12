c2f1e1c9b48e09088f6f450d8301a7309d21c83f
Chaban Bolboi <tizhin-ss@yandex.ru>
2023-07-12 23:14:16 +0700

    Initial commit

---
+++ src/main/java/ru/aisylu/mydemo2/Mydemo2Application.java
@@ -0,0 +1,13 @@
+package ru.aisylu.mydemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mydemo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mydemo2Application.class, args);
+    }
+
+}
